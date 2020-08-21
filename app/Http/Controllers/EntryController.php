906a869949bf26ab58c25b619f83f4d841bb0039
davepenha <jpenha1219@gmail.com>
2020-08-21 12:57:05 -0500

    agregando la segunda parte

---
+++ app/Http/Controllers/EntryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Entry;
+use Illuminate\Http\Request;
+
+class EntryController extends Controller
+{   
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function store(Request $request){
+        $valitedData = $request->validate([
+            'title' => 'required|min:7|max:100|unique:entries',
+            'content' =>'required|min:10|max:3000'
+        ]);
+
+        $entry = new Entry();
+        $entry->title = $valitedData['title'];
+        $entry->content = $valitedData['content'];
+        $entry->user_id = auth()->id();
+        $entry->save();
+
+        $status = 'Your entry has been published successfully';
+        return back()->with(compact('status'));
+    }
+    public function create(){
+        return view('entries.create');
+    }
+    public function edit(Entry $entry){
+        return view('entries.edit', compact('entry'));
+    }
+
+    public function update(Request $request, Entry $entry){
+        $valitedData = $request->validate([
+            'title' => 'required|min:7|max:100|unique:entries,id,'.$entry->id,
+            'content' =>'required|min:10|max:3000'
+        ]);
+
+        $entry->title = $valitedData['title'];
+        $entry->content = $valitedData['content'];
+        $entry->save();
+
+        $status = 'Your entry has been update successfully';
+        return back()->with(compact('status'));
+    }
+   
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Entry;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,7 +23,8 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        return view('home');
+    {   
+        $entries = Entry::where('user_id',auth()->id())->get();
+        return view('home',compact('entries'));
     }
 }
